0d36cd2ce305f28409693de0b1a61a92ddaceb90
Santiago Rodriguez <130719041+Opposin@users.noreply.github.com>
2023-07-25 19:18:56 -0300

    deleted unused script in js and unused class in css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
 document.getElementById("myButton").onclick = function () {
     location.href = "https://store.steampowered.com/app/70/HalfLife/";
 };
-
-window.addEventListener("scroll", function(){
-    var header = document.querySelector("header")
-    header.classList.toggle("sticky", window.scrollY > 15)
-})
